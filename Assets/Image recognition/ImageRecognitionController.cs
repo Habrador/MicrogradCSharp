c93f0e973165451b7dce6bc98cb84e0183bd20e7
habrador <erik.nordeus@gmail.com>
2025-06-25 13:23:23 +0200

    Added so you can display numbers on a quad

--- Assets/Image recognition/ImageRecognitionController.cs
+++ Assets/Image recognition/ImageRecognitionController.cs
@@ -3,8 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using UnityEngine;
+using Micrograd;
 
 //Train a neural network to recognize handwritten digits using autograd
 public class ImageRecognitionController : MonoBehaviour
@@ -17,7 +17,9 @@ public class ImageRecognitionController : MonoBehaviour
     {
         //Use mnist data (minimized version with 100 training and 10 test)
         //csv file, comma-separated
-        //Each row consists of 785 values: the first value is the label (0 to 9) and the remaining 784 values are the pixel values (0 to 255)
+        //Each row consists of 785 values:
+        //- the first value is the label (0 to 9)
+        //- the remaining 784 (28x28) values are the pixel values (0 to 255)
         string[] trainingDataStringArray = GetRawData("mnist-100", "mnist_train_100.csv");
 
         Debug.Log(trainingDataStringArray[0]);
@@ -25,7 +27,90 @@ public class ImageRecognitionController : MonoBehaviour
 
         GetData(trainingDataStringArray, out float[][] inputDataFloat, out float[] outputDataFloat);
 
+        //Normalize 
+        //Cant do that in GetData because we might not know the max and min values
+        //We know inputdata is grayscale number 0 -> 255
+        MinMax currentRange = new(0f, 255f);
+        //The range we want the data to be in
+        MinMax wantedRange = new(0f,1f);
 
+        float[][] normalizedInputDataFloat = NormalizeData(inputDataFloat, wantedRange, currentRange);
+
+        //Do we need to normalize the output as well???
+
+
+        //Display the data
+        int displayIndex = 6;
+
+        DisplayDigitOnQuad(inputDataFloat[displayIndex]);
+
+        Debug.Log(outputDataFloat[displayIndex]);
+    }
+
+
+
+    //Display data on a quad so we can see how the numbers looks like
+    private void DisplayDigitOnQuad(float[] grayScaleValues)
+    {
+        int textureSize = 28;
+
+        //Unity wants color in 0-1 range
+        MinMax currentRange = new(0f, 255f);
+        MinMax wantedRange = new(0f, 1f);
+
+        //Create a new texture
+        Texture2D texture = new(textureSize, textureSize)
+        {
+            filterMode = FilterMode.Point
+        };
+
+        //Convert grayscale data to a texture
+        for (int y = 0; y < textureSize; y++)
+        {
+            for (int x = 0; x < textureSize; x++)
+            {
+                //Calculate the index in the 1D array
+                int index = y * textureSize + x;
+                
+                //SHould be in 0-1 range, not 0-255
+                float value = DataManager.Remap(grayScaleValues[index], currentRange, wantedRange);
+
+                //Invert color which maybe looks better
+                value = 1f - value;
+
+                //Invert the y-coordinate to correct the orientation - otherwise it's upside down
+                texture.SetPixel(x, textureSize - 1 - y, new Color(value, value, value));
+            }
+        }
+
+        texture.Apply();
+
+        //Assign texture to material
+        Material quadMaterial = displayQuadObj.GetComponent<MeshRenderer>().material;
+
+        quadMaterial.mainTexture = texture;
+    }
+
+
+
+    //Normalize data array from one range to another
+    private float[][] NormalizeData(float[][] inputData, MinMax wantedRange, MinMax currentRange)
+    {
+        float[][] normalizedData = new float[inputData.Length][];
+        
+        for (int i = 0; i < inputData.Length; i++)
+        {
+            float[] normalizedSubData = new float[inputData[i].Length];
+
+            for (int j = 0; j < inputData[i].Length; j++)
+            {
+                normalizedSubData[j] = DataManager.Remap(inputData[i][j], currentRange, wantedRange);
+            }
+
+            normalizedData[i] = normalizedSubData;
+        }
+
+        return normalizedData;
     }
 
 
--- Assets/_MicrogradCSharp/Data/DataManager.cs
+++ Assets/_MicrogradCSharp/Data/DataManager.cs
@@ -8,11 +8,11 @@ namespace Micrograd
     //Standardized methods that will make it easier to handle data when training and using Neural Networks
     public class DataManager
     {
-        //Remap value from range 1 to range 2
+        //Remap value from current to wanted
         //We often need to remap input data to range -1 -> 1
-        public static float Remap(float value, MinMax range1, MinMax range2)
+        public static float Remap(float value, MinMax currentRange, MinMax wantedRange)
         {
-            float remappedValue = range2.min + (value - range1.min) * ((range2.max - range2.min) / (range1.max - range1.min));
+            float remappedValue = wantedRange.min + (value - currentRange.min) * ((wantedRange.max - wantedRange.min) / (currentRange.max - currentRange.min));
 
             return remappedValue;
         }
