3f30034a461c8969402e3611090d0aa2e5aa8aeb
habrador <erik.nordeus@gmail.com>
2025-08-07 09:55:28 +0200

    Added a Permutator class and methods to make it easier to work with batches

--- Assets/_MicrogradCSharp/Data/DataManager.cs
+++ Assets/_MicrogradCSharp/Data/DataManager.cs
@@ -16,5 +16,51 @@ namespace Micrograd
 
             return remappedValue;
         }
+
+
+
+        //
+        // Batches
+        //
+
+        public static int GetNumberOfBatches(int batchSize, int dataSize)
+        {
+            int numberOfBatches = (int)System.Math.Ceiling((double)dataSize / batchSize);
+
+            return numberOfBatches;
+        }
+
+
+
+        //Get the start and end index of a batch in the array with all data
+        //0 1 2 3 4 5 6 7 8 9 10 11
+        //batchSize = 5
+        //dataLength = 12
+        //-> batches = 3
+        //
+        //Batch 0
+        //batchStartIndex = 0 * 5 = 0
+        //batchEndIndex = 0 + 5 - 1 = 4
+        //actualBatchSize = 5
+        //
+        //Batch 1
+        //batchStartIndex = 1 * 5 = 5
+        //batchEndIndex = 5 + 5 - 1 = 9
+        //actualBatchSize = 5
+        //
+        //Batch 2
+        //batchStartIndex = 2 * 5 = 10
+        //batchEndIndex = 10 + 5 - 1 = 14 -> 12 - 1 = 11
+        public static void GetBatchStartAndEndIndex(int batchNumber, int batchSize, int dataLength, out int batchStartIndex, out int batchEndIndex)
+        {
+            batchStartIndex = batchNumber * batchSize;
+
+            batchEndIndex = batchStartIndex + batchSize - 1;
+
+            if (batchEndIndex >= dataLength)
+            {
+                batchEndIndex = dataLength - 1;
+            }
+        }
     }
 }
---
+++ Assets/_MicrogradCSharp/Data/Permutator.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Micrograd
+{
+    //Instead of shuffling the actual array with the data, which might be slow, we shuffle an array with indicies
+    //When we need an index, we take and index from here instead of the actual index
+    //actualArray: [3.23, 5.65, 7.65]
+    //shuffledIndices: [1, 0, 2]
+    //randomItem0 = actualArray[shuffledArray[0]] = 5.65
+    public class Permutator
+    {
+        private readonly int[] indices;
+
+        private readonly System.Random rng;
+
+        //Get a shuffled index
+        public int this[int i] => indices[i];
+
+
+
+        public Permutator(int size, System.Random rng = null)
+        {
+            this.indices = Enumerable.Range(0, size).ToArray();
+
+            this.rng = rng;
+
+            Shuffle();
+        }
+
+
+
+        //Shuffe the array again
+        public void Permute()
+        {
+            Shuffle();
+        }
+
+
+
+        //Shuffle using the Fisher-Yates shuffle algorithm: https://www.dotnetperls.com/fisher-yates-shuffle
+        private void Shuffle(System.Random rng = null)
+        {
+            int n = indices.Length;
+
+            if (rng == null)
+            {
+                rng = new System.Random();
+            }
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int swapIndex = i + rng.Next(0, n - i);
+
+                //Swap
+                (indices[i], indices[swapIndex]) = (indices[swapIndex], indices[i]);
+            }
+        }
+    }
+}
